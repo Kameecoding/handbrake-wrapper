3e326f7609ded4582c6a4ed4be88edc36b1ca322
Kameecoding <andrej.kovac.ggc@gmail.com>
2018-01-23 12:13:05 +0100

    CHange license headers

--- src/main/java/com/kameecoding/handbrake/Handbrake.java
+++ src/main/java/com/kameecoding/handbrake/Handbrake.java
@@ -1,3 +1,26 @@
+/** 
+ * MIT License 
+ * 
+ * Copyright (c) 2018 Andrej Kovac (Kameecoding) 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal 
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions: 
+ *  
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE. 
+ */ 
 package com.kameecoding.handbrake;
 
 import java.io.BufferedReader;
@@ -13,13 +36,9 @@ import java.util.regex.Pattern;
 import org.apache.commons.io.FileUtils;
 
 /**
- * Created by Andrej Kovac (kameecoding) <andrej.kovac.ggc@gmail.com> on 2017-06-17.
- */
-
-/**
  * Class for invoking handbrake process
  * 
- * @author KameeCoding
+ * @author Andrej Kovac (kameecoding) <andrej.kovac.ggc@gmail.com> on 2017-06-17.
  */
 public class Handbrake implements Runnable {
 	
